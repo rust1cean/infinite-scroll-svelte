772d80af9066975ecf56a3f4bf9bf65c9a9a5fea
rust1cean <az.az.az.az.75007500@gmail.com>
2025-01-10 13:32:52 +0500

    Fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,11 +2,11 @@ import { ONE_SECOND } from './constants.js';
 
 export const genRandomId = (): string => Math.round(Math.random() * 10_000).toString();
 
-export const promiseWithTimeout = (fn: () => Promise<any>, timeoutFailSecs: number) => {
+export const promiseWithTimeout = (fn: () => Promise<any>, timeoutFailInSecs: number) => {
 	return new Promise((res, rej) => {
-		const finalTimeout = timeoutFailSecs * ONE_SECOND;
+		const finalTimeout = timeoutFailInSecs * ONE_SECOND;
 		const timer = setTimeout(() => {
-			rej(new Error(`Promise timed out after ${timeoutFailSecs} second(s).\n${fn}`));
+			rej(new Error(`Promise timed out after ${timeoutFailInSecs} second(s).\n${fn}`));
 		}, finalTimeout);
 
 		fn()
